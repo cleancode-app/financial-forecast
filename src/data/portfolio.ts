d153098d5e4dcbdd99d2462ccf98c7889c30c3fa
Luke Thompson <endeavour9@gmail.com>
2026-07-11 19:32:32 +1000

    update schema

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -9,13 +9,21 @@ export interface Portfolio {
 export interface Asset {
     id: string; //class-specific uuid e.g. ASX:IOZ
     name: string;
+    location: string;
     assetClass: "shares" | "property" | "cash" | "bonds";
     expectedGrowthPerAnnum: number; //0-1
     expectedReturnPerAnnum: number; //0-1
+    valuations: AssetValue[]
+}
+
+export interface AssetValue {
+    date: string; // ISO8601 date UTC
+    value: number; //AUDs
 }
 
 export interface AssetPurchase {
     assetId: string;
+    unitsPurchased: number;
     price: number; //AUD e.g. 200000.01
     date: string; // ISO8601 date UTC
 }
